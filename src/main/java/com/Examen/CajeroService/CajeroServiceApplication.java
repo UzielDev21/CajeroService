0ed2e50be6ea140f95598e48115acb0380e30f17
digis <digis@ALIEN27>
2026-01-20 17:25:53 -0600

    Inicio de cajeroService - conexión a la BD

---
+++ src/main/java/com/Examen/CajeroService/CajeroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Examen.CajeroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CajeroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CajeroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Examen/CajeroService/CajeroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Examen.CajeroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajeroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
